a5ef8222ebb5ea1640bdf98663d9cbfd8d2cd413
Saimen-Luo <31783769+Saimen-Luo@users.noreply.github.com>
2019-11-18 10:37:39 +0800

    feat: 完成静态页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'todomvc-app-css/index.css'
+import 'todomvc-common/base.css'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <section className="todoapp">
+                <header className="header">
+                    <h1>todos</h1>
+                    <input className="new-todo" placeholder="What needs to be done?" autoFocus/>
+                </header>
+                <section className="main">
+                    <input id="toggle-all" className="toggle-all" type="checkbox"/>
+                    <label htmlFor="toggle-all">Mark all as complete</label>
+                    <ul className="todo-list">
+                        <li className="completed">
+                            <div className="view">
+                                <input className="toggle" type="checkbox" checked/>
+                                <label>Taste JavaScript</label>
+                                <button className="destroy"></button>
+                            </div>
+                            <input className="edit" value="Create a TodoMVC template"/>
+                        </li>
+                        <li>
+                            <div className="view">
+                                <input className="toggle" type="checkbox"/>
+                                <label>Buy a unicorn</label>
+                                <button className="destroy"></button>
+                            </div>
+                            <input className="edit" value="Rule the web"/>
+                        </li>
+                    </ul>
+                </section>
+                <footer className="footer">
+                    <span className="todo-count"><strong>0</strong> item left</span>
+                    <ul className="filters">
+                        <li>
+                            <a className="selected" href="#/">All</a>
+                        </li>
+                        <li>
+                            <a href="#/active">Active</a>
+                        </li>
+                        <li>
+                            <a href="#/completed">Completed</a>
+                        </li>
+                    </ul>
+                    <button className="clear-completed">Clear completed</button>
+                </footer>
+            </section>
+            <footer className="info">
+                <p>Double-click to edit a todo</p>
+                <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+                <p>Created by <a href="http://todomvc.com">you</a></p>
+                <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
